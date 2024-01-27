4d179b53a813170b143a00cf891a20cb98309dbf
samisuleman <samisuleman10@gmail.com>
2024-01-27 17:10:21 +0100

    Cleaned comments

--- Assets/Scripts/ParseClass.cs
+++ Assets/Scripts/ParseClass.cs
@@ -18,39 +18,7 @@ namespace GrandpaVisit
                 Debug.Log(block.id);
                 parsedBlocks.Add(block.id, block);
             }
-            
-            /*string[] files = Directory.GetFiles(inputFolder);
-            foreach (string file in files)
-            {
-                Debug.Log(file);
-                TextAsset targetFile = Resources.Load<TextAsset>("Blocks/" + Path.GetFileNameWithoutExtension(file));
-                Block block = JsonUtility.FromJson<Block>(targetFile.text);
-                parsedBlocks.Add(block.id, block);
-            }*/
-/*
-                try
-                {
-                    using (var sr = new StreamReader(file))
-                    {
-                        // string jsonToRead = "{\"text\": \"Blub\", \"headline\": \"Blu\", \"id\": \"Bb\", \"options\": []}";//File.ReadAllText(file);
-                        string jsonToRead = sr.ReadToEnd();
-                        // string contentData = sr.ReadToEnd();
-                        Debug.Log(jsonToRead);
-                        
-                        // json = System.Text.Encoding.UTF8.GetString(contentData.bytes, 3, contentData.bytes.Length - 3);
-                        Block block = JsonUtility.FromJson<Block>(jsonToRead);
-                        parsedBlocks.Add(block.id, block);
-                    }
-                }
-                catch (IOException e)
-                {
-                    Console.WriteLine("The file could not be read:");
-                    Console.WriteLine(e.Message);
-                }
-            }
-  */          return parsedBlocks;
+            return parsedBlocks;
         }
-        
-        
     }
 }

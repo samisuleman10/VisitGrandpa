75f855063825c53fc9e1161926e9cdc1ca5cabab
samisuleman <samisuleman10@gmail.com>
2024-01-27 19:51:31 +0100

    2 Situations

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,10 +6,22 @@ namespace GrandpaVisit
     public class Block
     {
         public Reaction[] Reactions;
-        public string text;
-        public int Unit;
+        //public string text;
+        public int Unit = -1;
         public string id;
         public Option[] Actions;
+        
+        public Reaction GetReaction(int points)
+        {
+            foreach (var reaction in Reactions)
+            {
+                if (reaction.minPointsIncl <= points && reaction.maxPointsIncl >= points)
+                {
+                    return reaction;
+                }
+            }
+            return null;
+        }
     }
 
     [Serializable]
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using RedBlueGames.Tools.TextTyper;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GrandpaVisit
 {
@@ -8,12 +12,22 @@ namespace GrandpaVisit
     {
         public static GameManager Instance { get; private set; }
         public Dictionary<string, Block> Blocks = new Dictionary<string, Block>();
+        public TextTyper TextTyperSituation;
+        public int[] minPointRequirements;
+        public string[] startIds;
+        public string[] mainIds = new string[6];
+        public int Points;
+        private int unit = 0;
+
+        public GameObject ActionButtonPrefab;
+        public Transform ActionButtonParent;
+        GameObject actionButton;
         private void Awake()
         {
             if (Instance == null)
             {
                 Instance = this;
-                DontDestroyOnLoad(gameObject);
+                // DontDestroyOnLoad(gameObject);
             }
             else
             {
@@ -25,8 +39,116 @@ namespace GrandpaVisit
         {
             var parseClass = new ParseClass();
             Blocks = parseClass.ReadAllBlocks("Blocks");
+            buildMainIds();
+            DisplayBlock(startIds[0]);
         }
-    
+
+        private void DisplayBlock(string id)
+        {
+            Debug.Log(id);
+            TextTyperSituation.TypeText(Blocks[id].GetReaction(Points).text);
+            if (actionButton!= null)
+            {
+                foreach (Transform child in ActionButtonParent)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+
+            foreach (var option in Blocks[id].Actions)
+            {
+                actionButton = Instantiate(ActionButtonPrefab, ActionButtonParent);
+                actionButton.GetComponentInChildren<TextMeshProUGUI>().text = option.text;
+                actionButton.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    OnActionClicked(option);
+                });
+            }
+        }
+        
+        private void OnActionClicked(Option option)
+        {
+            Debug.Log("Action clicked" + option.text);
+            Points += option.points;
+            if (string.IsNullOrEmpty(option.followup))
+            {
+                if (checkUnitSwitched(Points))
+                {
+                    DisplayBlock(startIds[unit]);
+                }
+                else
+                {
+                    DisplayBlock(getMainId(Points));
+                }
+                
+            }
+            else
+            {
+                DisplayBlock(option.followup);
+            }
+        }
+
+        private string getMainId(int points)
+        {
+            for (int i = 0; i < minPointRequirements.Length; i++)
+            {
+                if (points >= minPointRequirements[i])
+                {
+                    return mainIds[i];
+                }
+            }
+
+            return null;
+        }
+
+        private bool checkUnitSwitched(int points)
+        {
+            if (points >= minPointRequirements[unit + 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void buildMainIds()
+        {
+            foreach (var block in Blocks.Values)
+            {
+                if (block.Unit >= 0)
+                {
+                    mainIds[block.Unit] = block.id;
+                }
+            }
+        }
+
+        private void GetJson()
+        {
+            Block myBlock = new Block
+            {
+                // text = "Example block text",
+                Unit = 1,
+                id = "block1",
+                Reactions = new Reaction[]
+                {
+                    new Reaction { minPointsIncl = 0, maxPointsIncl = 10, text = "Reaction 1" },
+                    new Reaction { minPointsIncl = 11, maxPointsIncl = 20, text = "Reaction 2" }
+                },
+                Actions = new Option[]
+                {
+                    new Option { text = "Option 1", followup = "block2", points = 5 },
+                    new Option { text = "Option 2", followup = "block3", points = 10 }
+                }
+            };
+
+            // Convert to JSON
+            string json = JsonUtility.ToJson(myBlock);
+
+            // Output the JSON string
+            Debug.Log(json);
+        }
     }
 }
 

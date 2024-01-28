53fdcf856d7dd13e32759d761c1e28b2f66f2dea
Tobias Kohl <tobiko10@aol.com>
2024-01-28 14:30:15 +0100

    introduce randomness when multiple eligible answers

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GrandpaVisit
 {
     [Serializable]
     public class Block
     {
+        private static Random rnd = new Random();
+
         public Reaction[] Reactions;
         //public string text;
         public int Unit = -1;
@@ -13,15 +16,22 @@ namespace GrandpaVisit
         
         public Reaction GetReaction(int points)
         {
+            List<Reaction> suitableReactions = new List<Reaction>();
             foreach (var reaction in Reactions)
             {
                 if (reaction.minPointsIncl <= points && reaction.maxPointsIncl >= points)
                 {
-                    return reaction;
+                    suitableReactions.Add(reaction);
                 }
             }
+            if (suitableReactions.Count > 0)
+			{
+                int index = rnd.Next(0, suitableReactions.Count);
+                return suitableReactions[index];
+			}
             return null;
         }
+        
     }
 
     [Serializable]
